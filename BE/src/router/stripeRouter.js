5c999c372b2047e23483dc11e33ecd9ee8e11093
Dang Minh Duc <112186500+ducwhite@users.noreply.github.com>
2023-04-12 09:07:09 +0700

    Delete stripeRouter.js

--- BE/src/router/stripeRouter.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const stripeController = require('../controller/stripeController');
-
-router.post('/create-checkout-session', stripeController.postStripe);
-router.post('/webhook',express.raw({type: 'application/json'}),stripeController.postSuccess);
-
-module.exports = router;
